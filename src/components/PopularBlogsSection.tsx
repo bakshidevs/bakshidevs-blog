b8b3c5eb4cab029ad89b0fd6a409edcf08f0a41c
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-10 23:18:41 +0530

    PopularBlogs component for home page

---
+++ src/components/PopularBlogsSection.tsx
@@ -0,0 +1,21 @@
+import { allBlogs } from "../pages/AllBlogs"
+import BlogCard from "./BlogCard"
+
+export default function PopularBlogsSection() {
+    return (
+        <section aria-label="popular-blogs">
+            <div className="flex justify-between items-center text-accent py-2">
+                <h1 className="font-bold text-2xl">Popular Blogs</h1>
+                <button className="px-2 py-1 bg-primary hover:bg-accent hover:text-primary transition-all duration-300 font-medium rounded-md">View All</button>
+            </div>
+            <div className="flex gap-4 overflow-x-auto scroll-smooth snap-x snap-mandatory py-6 styled-scrollbar">
+                {allBlogs.slice(1, 8).map((blog) => (
+                    <div key={blog.slug} className="flex-none snap-center">
+                        <BlogCard blog={blog} />
+                    </div>
+                ))}
+            </div>
+
+        </section>
+    )
+}
