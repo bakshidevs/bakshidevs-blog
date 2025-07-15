e86c798bc1fec40b5b70184413ba6df801dbcb08
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-15 23:13:56 +0530

    added dark and light mode to both the editor and preview, added few more inputs for heading, thumbnail and slug

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,6 +1,6 @@
 import MDEditor from '@uiw/react-md-editor';
-import { useState } from 'react';
 import remarkGfm from 'remark-gfm';
+import { useState } from 'react';
 import useThemeStore from '../store/themeStore.ts';
 import useBlogStore from '../store/blogStore.ts';
 import useAuthStore from '../store/authStore.ts';
@@ -16,6 +16,7 @@ interface EditorBlogDraft {
 // #TODO: Figure out a way to fix this page styling along with the Markdown editor or find replacement
 
 export default function TextEditor() {
+    const [blogTitle, setBlogTitle] = useState<string | undefined>("")
     const [blog, setBlog] = useState<EditorBlogDraft>({
         title: "",
         slug: "",
@@ -30,7 +31,7 @@ export default function TextEditor() {
         // Join the first two sentences
         return sentences.slice(0, 2).join("").trim();
     }
-    const { theme } = useThemeStore()
+    const { theme, isDarkModeEnabled } = useThemeStore()
     const { createBlog } = useBlogStore();
     const { user } = useAuthStore()
     const { editorValue, setEditorValue } = useMDEditorStore();
@@ -58,27 +59,52 @@ export default function TextEditor() {
         <div className="h-full relative">
             <h1 className="font-bold text-3xl mt-6 mb-2">Create a New Post</h1>
             <p className="mb-6">Enrich people with knowledge.</p>
-            <MDEditor
-                value={editorValue}
-                onChange={setEditorValue}
-                style={{
-                    minHeight: "600px",
-                    backgroundColor: theme === "light" ? "transparent" : "#0d1117",
-                    color: theme === "light" ? "#000" : "#B4637A",
-                    // color: "#FAF4ED"
-                }}
-                previewOptions={{
-                    remarkPlugins: [remarkGfm],
-                    style: {
-                        backgroundColor: theme === "light" ? "#f1f1f1" : "#0d1117",
-                        color: theme === "light" ? "#c1c1c1" : "#B4637A",
-                    }
-                }}
-            />
-            <div className="h-100 bg-accent/10 w-full"></div>
-            <div className="flex gap-4 font-medium fixed bottom-12 right-8">
+            <div className="mb-16">
+                <div className="grid gap-2">
+                    <input
+                        className="bg-accent/10 px-3 py-5 outline-0 focus:outline-1 focus:outline-accent"
+                        value={blogTitle}
+                        onChange={(e) => setBlogTitle(e.target.value)}
+                        aria-label="blog-title-input"
+                        type="text"
+                        name="Blog-Title"
+                        placeholder="Enter your blog title..."
+                    />
+                    <input
+                        value={blogTitle}
+                        className="bg-accent/10 p-3"
+                        type="text"
+                        disabled
+                    />
+                    <input className="bg-accent/10 p-3 cursor-pointer" placeholder='add your thumbnail' type="file" name="thumbnail" />
+                </div>
+                <MDEditor
+                    value={editorValue}
+                    onChange={setEditorValue}
+                    data-color-mode={theme}
+                    components={{}}
+                    style={{
+                        minHeight: "650px",
+                        color: isDarkModeEnabled ? "#B4637A " : "",
+                    }}
+                    previewOptions={{
+                        remarkPlugins: [remarkGfm],
+                        style: {
+
+                            color: isDarkModeEnabled ? "#B4637A " : ""
+                        }
+                    }}
+                />
+                {/* <MDEditor.Markdown
+                    wrapperElement={{
+                        'data-color-mode': isDarkModeEnabled ? "dark" : "light",
+                    }}
+                    source={editorValue}
+                /> */}
+            </div>
+            <div className="flex gap-4 font-medium absolute bottom-12 right-8">
                 <button className="hover:bg-olive/80 bg-olive active:scale-105 transition-all duration-200 px-2 py-1 rounded-md">Save to Drafts</button>
-                <button className="px-2 py-1 rounded-md bg-accent hover:bg-accent/80 active:scale-105 transition-all duration-200">Post</button>
+                <button onClick={handleCreateBlog} className="px-2 py-1 rounded-md bg-accent hover:bg-accent/80 active:scale-105 transition-all duration-200">Post</button>
             </div>
         </div>
     )
--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,40 +1,9 @@
-import { useState } from "react";
-
-import DOMPurify from "dompurify";
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import TextEditor from "../components/TextEditor.tsx";
 
 export default function Write() {
-  const [text, setText] = useState<string>("");
   return (
-    <TabGroup>
-      <TabList className="flex">
-        <Tab className="flex-1 text-center py-2 px-4 border-b-2 data-hover:bg-olive/30 data-selected:bg-accent/10">
-          Write
-        </Tab>
-        <Tab className="flex-1 text-center py-2 px-4 border-b-2 data-hover:bg-olive/30 data-selected:bg-accent/10">
-          Preview
-        </Tab>
-      </TabList>
-      <TabPanels className="mt-4">
-        <TabPanel>
-          <h1 className="py-4 font-bold text-2xl">HTML Editor</h1>
-          <textarea
-            className="w-full h-96 p-4 border border-gray-300 dark:border-gray-700 rounded-md"
-            placeholder="Write your HTML content here..."
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          ></textarea>
-        </TabPanel>
-        <TabPanel>
-          <div className="my-4">
-            <h2 className="text-xl font-semibold mb-2">Preview</h2>
-            <div
-              className="p-4 border min-h-96 border-gray-300 dark:border-gray-700 rounded-md"
-              dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(text) }}
-            ></div>
-          </div>
-        </TabPanel>
-      </TabPanels>
-    </TabGroup>
+    <div className="h-full">
+      <TextEditor />
+    </div>
   )
 }
