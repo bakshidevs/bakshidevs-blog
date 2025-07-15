043971915a96e8ded16f9d8f2eee1bf8410486c0
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-15 23:15:03 +0530

    auth memory changed to localstorage

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -105,7 +105,7 @@ const useAuthStore = create<AuthState & AuthActions>()(
     }),
         {
             name: "auth-storage",
-            storage: createJSONStorage(() => sessionStorage),
+            storage: createJSONStorage(() => localStorage),
         }
     )
 )
