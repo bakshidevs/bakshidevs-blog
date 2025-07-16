f1f7a1cb82c0974838057ccaf38b0083a2ff195d
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-16 23:28:00 +0530

    - added title for blog - store updated to handle tittle

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -16,7 +16,7 @@ interface EditorBlogDraft {
 // #TODO: Figure out a way to fix this page styling along with the Markdown editor or find replacement
 
 export default function TextEditor() {
-    const [blogTitle, setBlogTitle] = useState<string | undefined>("")
+
     const [blog, setBlog] = useState<EditorBlogDraft>({
         title: "",
         slug: "",
@@ -34,7 +34,7 @@ export default function TextEditor() {
     const { theme, isDarkModeEnabled } = useThemeStore()
     const { createBlog } = useBlogStore();
     const { user } = useAuthStore()
-    const { editorValue, setEditorValue } = useMDEditorStore();
+    const { blogTitle, setBlogTitle, editorValue, setEditorValue } = useMDEditorStore();
     const handleCreateBlog = () => {
         try {
             createBlog({
@@ -55,14 +55,15 @@ export default function TextEditor() {
 
         }
     }
+    const slug = blogTitle.toLowerCase().replace(/[^a-z0-9\s]/g, '').trim().replace(/\s+/g, '-');
     return (
-        <div className="h-full relative">
+        <div className="h-full w-9/10 mx-auto relative">
             <h1 className="font-bold text-3xl mt-6 mb-2">Create a New Post</h1>
             <p className="mb-6">Enrich people with knowledge.</p>
             <div className="mb-16">
                 <div className="grid gap-2">
                     <input
-                        className="bg-accent/10 px-3 py-5 outline-0 focus:outline-1 focus:outline-accent"
+                        className="bg-accent/10 px-3 py-5 outline-0 focus:outline-1 focus:outline-accent rounded-md"
                         value={blogTitle}
                         onChange={(e) => setBlogTitle(e.target.value)}
                         aria-label="blog-title-input"
@@ -71,12 +72,12 @@ export default function TextEditor() {
                         placeholder="Enter your blog title..."
                     />
                     <input
-                        value={blogTitle}
-                        className="bg-accent/10 p-3"
+                        value={slug}
+                        className="bg-accent/10 p-3 rounded-md"
                         type="text"
                         disabled
                     />
-                    <input className="bg-accent/10 p-3 cursor-pointer" placeholder='add your thumbnail' type="file" name="thumbnail" />
+                    <input className="bg-accent/10 p-3 rounded-md cursor-pointer" placeholder='add your thumbnail' type="file" name="thumbnail" />
                 </div>
                 <MDEditor
                     value={editorValue}
@@ -95,12 +96,16 @@ export default function TextEditor() {
                         }
                     }}
                 />
-                {/* <MDEditor.Markdown
-                    wrapperElement={{
-                        'data-color-mode': isDarkModeEnabled ? "dark" : "light",
-                    }}
-                    source={editorValue}
-                /> */}
+                <div className="grid">
+                    <h1 className="font-bold text-4xl text-[#c9d1d9] my-4">{blogTitle}</h1>
+                    <hr className='h-0.5 text-[#c9d1d9] mb-2' />
+                    <MDEditor.Markdown
+                        wrapperElement={{
+                            'data-color-mode': isDarkModeEnabled ? "dark" : "light",
+                        }}
+                        source={editorValue}
+                    />
+                </div>
             </div>
             <div className="flex gap-4 font-medium absolute bottom-12 right-8">
                 <button className="hover:bg-olive/80 bg-olive active:scale-105 transition-all duration-200 px-2 py-1 rounded-md">Save to Drafts</button>
--- src/store/editorStore.ts
+++ src/store/editorStore.ts
@@ -2,17 +2,23 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 type State = {
+    blogTitle: string;
     editorValue: string | undefined;
 }
 
 type Action = {
+    setBlogTitle: (title: string | undefined) => void,
     setEditorValue: (editorValue: string | undefined) => void;
 }
 
 const useMDEditorStore = create<State & Action>()(
     persist((set) => (
         {
+            blogTitle: "Beginning from the End",
             editorValue: "# Start here",
+            setBlogTitle: (blogTitle: string | undefined) => {
+                set({ blogTitle })
+            },
             setEditorValue: (editorValue: string | undefined) => {
                 set({ editorValue })
             }
