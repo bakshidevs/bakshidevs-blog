94abaf78956eb6e096797d392f38fd72cb9e9362
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-19 14:02:38 +0530

    upload and update blogs

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,116 +1,135 @@
 import MDEditor from '@uiw/react-md-editor';
 import remarkGfm from 'remark-gfm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router';
 import useThemeStore from '../store/themeStore.ts';
-import useBlogStore from '../store/blogStore.ts';
+import useBlogStore, { type BlogType } from '../store/blogStore.ts';
 import useAuthStore from '../store/authStore.ts';
-import useMDEditorStore from '../store/editorStore.ts';
-
-interface EditorBlogDraft {
-    title: string;
-    slug: string;
-    tags: string[];
-    content: any;
-}
-
-// #TODO: Figure out a way to fix this page styling along with the Markdown editor or find replacement
+import { UploadCloud } from 'lucide-react';
 
 export default function TextEditor() {
+    const { slug } = useParams<{ slug: string }>();
+    const navigate = useNavigate();
+    const { theme } = useThemeStore();
+    const { createBlog, updateBlog, getBlogById, uploadThumbnail, currentBlog } = useBlogStore();
+    const { user } = useAuthStore();
 
-    const [blog, setBlog] = useState<EditorBlogDraft>({
-        title: "",
-        slug: "",
-        tags: [""],
-        content: ""
-    })
-    function getExcerpt(text: string | undefined): string {
-        // Match sentence endings: ., ?, or ! followed by a space or end of string
-        const sentences = text?.match(/[^.!?]+[.!?]+(\s|$)/g);
-        if (!sentences) return "";
+    const [blog, setBlog] = useState<Partial<BlogType>>({
+        title: '',
+        slug: '',
+        tags: [],
+        content: '',
+        image: '',
+        isPublished: false,
+        isDraft: false,
+    });
+    const [thumbnailFile, setThumbnailFile] = useState<File | null>(null);
 
-        // Join the first two sentences
-        return sentences.slice(0, 2).join("").trim();
-    }
-    const { theme, isDarkModeEnabled } = useThemeStore()
-    const { createBlog } = useBlogStore();
-    const { user } = useAuthStore()
-    const { blogTitle, setBlogTitle, editorValue, setEditorValue } = useMDEditorStore();
-    const handleCreateBlog = () => {
-        try {
-            createBlog({
-                title: blog.title,
-                createdAt: Date.now(),
-                author: user?.name,
-                excerpt: getExcerpt(editorValue),
-                tags: blog.tags,
-                slug: blog.slug,
-                content: editorValue,
-                readingTime: "5 mins",
-                isArchived: false,
-                isFeatured: false,
-                isDraft: false,
-                isPublished: true,
-            })
-        } catch (error) {
-
+    useEffect(() => {
+        if (slug) {
+            getBlogById(slug);
         }
-    }
-    const slug = blogTitle.toLowerCase().replace(/[^a-z0-9\s]/g, '').trim().replace(/\s+/g, '-');
+    }, [slug, getBlogById]);
+
+    useEffect(() => {
+        if (currentBlog && slug) {
+            setBlog(currentBlog);
+        }
+    }, [currentBlog, slug]);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setBlog((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleTagsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setBlog((prev) => ({ ...prev, tags: value.split(',').map(tag => tag.trim()) }));
+    };
+
+    const handleContentChange = (value?: string) => {
+        setBlog((prev) => ({ ...prev, content: value }));
+    };
+
+    const handleThumbnailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setThumbnailFile(e.target.files[0]);
+        }
+    };
+
+    const handleSave = async (isDraft: boolean) => {
+        let thumbnailUrl = blog.image;
+        if (thumbnailFile) {
+            thumbnailUrl = await uploadThumbnail(thumbnailFile);
+        }
+
+        const blogData = {
+            ...blog,
+            thumbnail: thumbnailUrl,
+            author: user?.$id,
+            isPublished: !isDraft,
+            isDraft,
+        };
+
+        if (slug) {
+            await updateBlog(slug, blogData);
+        } else {
+            await createBlog(blogData);
+        }
+        navigate('/profile');
+    };
+
     return (
         <div className="h-full w-9/10 mx-auto relative">
-            <h1 className="font-bold text-3xl mt-6 mb-2">Create a New Post</h1>
+            <h1 className="font-bold text-3xl mt-6 mb-2">{slug ? 'Edit Post' : 'Create a New Post'}</h1>
             <p className="mb-6">Enrich people with knowledge.</p>
             <div className="mb-16">
-                <div className="grid gap-2">
+                <div className="grid gap-4">
                     <input
                         className="bg-accent/10 px-3 py-5 outline-0 focus:outline-1 focus:outline-accent rounded-md"
-                        value={blogTitle}
-                        onChange={(e) => setBlogTitle(e.target.value)}
-                        aria-label="blog-title-input"
-                        type="text"
-                        name="Blog-Title"
+                        value={blog.title}
+                        onChange={handleInputChange}
+                        name="title"
                         placeholder="Enter your blog title..."
                     />
                     <input
-                        value={slug}
-                        className="bg-accent/10 p-3 rounded-md"
-                        type="text"
-                        disabled
+                        className="bg-accent/10 px-3 py-5 outline-0 focus:outline-1 focus:outline-accent rounded-md"
+                        value={blog.tags?.join(', ')}
+                        onChange={handleTagsChange}
+                        name="tags"
+                        placeholder="Tags (comma-separated)..."
                     />
-                    <input className="bg-accent/10 p-3 rounded-md cursor-pointer" placeholder='add your thumbnail' type="file" name="thumbnail" />
+                    <div className="bg-accent/10 mb-3 rounded-md flex justify-center items-center p-4">
+                        <label htmlFor="blog-thumbnail" className="rounded-md border-2 border-dotted border-accent flex flex-col items-center p-8 cursor-pointer">
+                            <UploadCloud />
+                            <p className="text-accent font-semibold">Click to browse or drag & drop</p>
+                            <p className="text-sm text-secondary/50 dark:text-primary/50">PNG, JPG, GIF</p>
+                            <input id="blog-thumbnail" className="hidden" type="file" onChange={handleThumbnailChange} />
+                        </label>
+                    </div>
                 </div>
                 <MDEditor
-                    value={editorValue}
-                    onChange={setEditorValue}
+                    value={blog.content}
+                    onChange={handleContentChange}
                     data-color-mode={theme}
-                    components={{}}
-                    style={{
-                        minHeight: "650px",
-                        color: isDarkModeEnabled ? "#B4637A " : "",
-                    }}
-                    previewOptions={{
-                        remarkPlugins: [remarkGfm],
-                        style: {
-
-                            color: isDarkModeEnabled ? "#B4637A " : ""
-                        }
-                    }}
+                    style={{ minHeight: '650px' }}
+                    previewOptions={{ remarkPlugins: [remarkGfm] }}
                 />
-                <div className="grid">
-                    <h1 className="font-bold text-4xl text-[#c9d1d9] my-4">{blogTitle}</h1>
-                    <hr className='h-0.5 text-[#c9d1d9] mb-2' />
-                    <MDEditor.Markdown
-                        wrapperElement={{
-                            'data-color-mode': isDarkModeEnabled ? "dark" : "light",
-                        }}
-                        source={editorValue}
-                    />
-                </div>
             </div>
-            <div className="flex gap-4 font-medium absolute bottom-12 right-8">
-                <button className="hover:bg-olive/80 bg-olive active:scale-105 transition-all duration-200 px-2 py-1 rounded-md">Save to Drafts</button>
-                <button onClick={handleCreateBlog} className="px-2 py-1 rounded-md bg-accent hover:bg-accent/80 active:scale-105 transition-all duration-200">Post</button>
+            <div className="flex gap-4 font-medium fixed bottom-24 right-8">
+                <button
+                    className="bg-olive active:scale-105 transition-all duration-200 px-2 py-1 rounded-md hover:bg-olive/80"
+                    onClick={() => handleSave(true)}
+                >
+                    Save to Drafts
+                </button>
+                <button
+                    onClick={() => handleSave(false)}
+                    className="px-2 py-1 rounded-md bg-accent active:scale-105 transition-all duration-200 hover:bg-accent/80"
+                >
+                    {slug ? 'Update Blog' : 'Publish Blog'}
+                </button>
             </div>
         </div>
-    )
+    );
 }
--- src/store/blogStore.ts
+++ src/store/blogStore.ts
@@ -1,34 +1,39 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-
-// importing databases and client from appwrite
-import { databases } from "../lib/appwrite";
-
-// importing conf from conf file
+import { databases, storage, ID } from "../lib/appwrite";
 import { conf } from "../conf/conf";
+import { Query } from "appwrite";
 
-interface Blog {
+export interface BlogType {
+    $id?: string;
     title: string;
     slug: string;
+    excerpt: string;
     content: string;
-    category: string;
+    image: string;
     tags: string[];
+    author: string;
     isPublished: boolean;
-    isDraft: boolean;
-    isArchived: boolean;
     isFeatured: boolean;
+    isDraft: boolean;
+    createdAt?: string;
+    readingTime: string;
+    updatedAt?: string;
+    isArchived: boolean;
 }
 
 type BlogState = {
-    blogs: Blog[];
+    blogs: BlogType[];
+    currentBlog: BlogType | null;
     isLoading: boolean;
 }
 
 type BlogActions = {
-    createBlog: (blog: Blog) => void;
-    updateBlog: (slug: string, updatedBlog: Partial<Blog>) => void;
-    deleteBlog: (slug: string) => void;
-    setBlogs: (blogs: Blog[]) => void;
+    uploadThumbnail: (file: File) => Promise<string | undefined>;
+    createBlog: (blog: Partial<BlogType>) => Promise<void>;
+    updateBlog: (blogId: string, blog: Partial<BlogType>) => Promise<void>;
+    getBlogById: (blogId: string) => Promise<void>;
+    getBlogsByAuthor: (authorId: string) => Promise<void>;
 }
 
 type BlogStore = BlogState & BlogActions;
@@ -37,37 +42,86 @@ const useBlogStore = create<BlogStore>()(
     persist(
         (set) => ({
             blogs: [],
+            currentBlog: null,
             isLoading: false,
-            createBlog: async ({ title, slug, content, isPublished, isDraft }) => {
+            uploadThumbnail: async (file: File) => {
+                try {
+                    const response = await storage.createFile(conf.appwriteBucketId, ID.unique(), file);
+                    const fileId = response.$id;
+                    const previewUrl = storage.getFilePreview(conf.appwriteBucketId, fileId);
+                    return previewUrl.toString();
+                } catch (error) {
+                    console.error("Upload failed :: Appwrite :: ", error);
+                }
+            },
+            createBlog: async (blog) => {
                 set({ isLoading: true });
                 try {
                     await databases.createDocument(
                         conf.appwriteDatabaseId,
                         conf.appwriteBlogsCollectionId,
-                        slug,
-                        {
-                            title,
-                            slug,
-                            content,
-                            isPublished,
-                            isDraft,
-                            createdAt: new Date().toISOString(),
-                            updatedAt: new Date().toISOString()
-                        }
-                    )
+                        ID.unique(),
+                        blog
+                    );
                 } catch (error) {
-
+                    console.error("Blog creation failure :: Appwrite :: ", error);
+                } finally {
+                    set({ isLoading: false });
                 }
             },
-            updateBlog: (slug, updatedBlog) => set((state) => ({
-                blogs: state.blogs.map((blog) =>
-                    blog.slug === slug ? { ...blog, ...updatedBlog } : blog
-                )
-            })),
-            deleteBlog: (slug) => set((state) => ({
-                blogs: state.blogs.filter((blog) => blog.slug !== slug)
-            })),
-            setBlogs: (blogs) => set({ blogs })
+            updateBlog: async (blogId, blog) => {
+                set({ isLoading: true });
+                try {
+                    await databases.updateDocument(
+                        conf.appwriteDatabaseId,
+                        conf.appwriteBlogsCollectionId,
+                        blogId,
+                        blog
+                    );
+                } catch (error) {
+                    console.error("Blog update failure :: Appwrite :: ", error);
+                } finally {
+                    set({ isLoading: false });
+                }
+            },
+            getBlogById: async (blogId) => {
+                set({ isLoading: true });
+                try {
+                    const response = await databases.getDocument(
+                        conf.appwriteDatabaseId,
+                        conf.appwriteBlogsCollectionId,
+                        blogId
+                    );
+                    if (response) {
+                        set({ currentBlog: response as any });
+                    } else {
+                        set({ currentBlog: null });
+                    }
+                } catch (error) {
+                    console.error("Blog fetching failed :: Appwrite :: ", error);
+                } finally {
+                    set({ isLoading: false });
+                }
+            },
+            getBlogsByAuthor: async (authorId) => {
+                set({ isLoading: true });
+                try {
+                    const response = await databases.listDocuments(
+                        conf.appwriteDatabaseId,
+                        conf.appwriteBlogsCollectionId,
+                        [Query.equal("author", authorId)]
+                    );
+                    if (response.documents) {
+                        set({ blogs: response.documents as any });
+                    } else {
+                        set({ blogs: [] });
+                    }
+                } catch (error) {
+                    console.error("Fetching user blogs failed :: Appwrite :: ", error);
+                } finally {
+                    set({ isLoading: false });
+                }
+            }
         }),
         {
             name: "blog-storage",
