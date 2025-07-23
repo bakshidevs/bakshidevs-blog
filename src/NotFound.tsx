8da012162f681ea4414c821d2f9e066d49dcb2e6
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-23 23:58:55 +0530

    Not found page updated for more user actions

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -7,10 +7,10 @@ export default function NotFound() {
   const { isAuthenticated } = useAuthStore()
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="w-2/3 md:max-w-1/3 p-4 border border-accent dark:border-accent/50 rounded-md bg-secondary/10 dark:bg-primary/10">
+      <div className="w-2/3 md:max-w-125 p-8 border border-accent dark:border-accent/50 rounded-md bg-secondary/10 dark:bg-primary/10">
         <Coffee className="w-12 h-12 p-2 pl-0" />
-        <h2 className="font-bold text-2xl underline">
-          Page Not Found!
+        <h2 className="font-bold text-3xl mb-4 underline text-accent/80 hover:text-accent underline-offset-4">
+          404 - Page Not Found!
         </h2>
         <p>
           The page you are looking for does not exist or has been moved.
@@ -18,7 +18,7 @@ export default function NotFound() {
         </p>
         {isAuthenticated ? (
           <p className="mt-4">
-            If you believe this is an error, please contact support.
+            If you believe this is an error, please <Link className="font-medium text-olive hover:text-accent underline" to="/contact">contact</Link> support.
           </p>
         ) : (
           <p className="mt-4">
