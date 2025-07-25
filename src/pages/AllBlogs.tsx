f1c4c7a2e66fd31bc9e6459d45fc984660bbe13c
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-25 22:33:31 +0530

    changed from flex to grid for better preview

--- src/pages/AllBlogs.tsx
+++ src/pages/AllBlogs.tsx
@@ -10,15 +10,14 @@ export default function AllBlogs() {
 
 
     return blogs.length > 0 ?
-        <div>
+        <div className="mb-12">
             <div className="w-full my-8 flex flex-col items-center justify-center h-64 border rounded border-accent bg-accent/10">
                 <div className="w-4/5 md:w-2/3 text-center">
                     <h1 className="font-bold text-accent text-3xl my-1">Tech Blogs</h1>
                     <p className="text-base">Insights, tutorials, and discussions on web development, programming, and technology.</p>
                 </div>
             </div>
-            <div className="mx-auto w-4/5 md:w-3/4 xl:w-2/3 flex gap-6 flex-wrap my-16 justify-center">
-
+            <div className="w-4/5 md:w-3/4 xl:w-2/3 mx-auto grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 4xl:grid-cols-4 gap-4">
                 {blogs.map((blog) => (
                     <Link key={blog.slug} to={`/blog/${blog.slug}`}>
                         <BlogCard blog={blog} />
