f2d708fad530a80c10c7db1e924190cf3566384b
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-25 22:28:51 +0530

    Changed popular blogs to Latest Blogs (yet to track user interactions)

---
+++ src/components/LatestBlogsSection.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router"
+import BlogCard from "./BlogCard.tsx"
+import useBlogStore from "../store/blogStore.ts"
+
+export default function LatestBlogsSection() {
+    const { blogs } = useBlogStore();
+
+    return blogs.length > 0 ? (
+        <section aria-label="popular-blogs">
+            <div className="flex justify-between items-center text-accent py-3">
+                <h2 className="font-bold text-2xl">Latest Blogs</h2>
+                <Link to="/blogs">
+                    <button className="px-2 py-1 bg-primary hover:bg-accent hover:text-primary transition-all duration-300 font-medium rounded-md">View All</button>
+                </Link>
+            </div>
+            <div className="w-full grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 4xl:grid-cols-4 gap-3">
+                {blogs.slice(1, 5).map((blog) => (
+                        <Link key={blog.slug} to={`/blog/${blog.slug}`}>
+                            <BlogCard blog={blog} />
+                        </Link>
+                ))}
+            </div>
+        </section>
+    ) : <>Hi</>
+}
