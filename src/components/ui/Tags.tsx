bba23b833813df421246cfb019138ec05b7c6cfe
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-25 22:35:04 +0530

    - added new breakpoint for smaller and larger display to have more responsiveness - made the tags little smaller

--- src/components/ui/Tags.tsx
+++ src/components/ui/Tags.tsx
@@ -1,7 +1,7 @@
 
 export default function Tags({ tag }: { tag: string }) {
     return (
-        <span className="px-3 py-1 bg-accent/10 dark:bg-accent/20 rounded-full text-xs font-medium text-accent dark:text-accent/80">
+        <span className="px-2 py-0.5 sm:px-3 sm:py-1 bg-accent/10 dark:bg-accent/20 rounded-full text-xs font-medium text-accent dark:text-accent/80">
             {tag}
         </span>
     )
