d533376d7c21033fb11d2e57ac05c43360c0efa9
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-29 00:36:29 +0530

    tags working, independent of text editor (delete tags yet to be implemented)

--- src/components/BlogTagsInput.tsx
+++ src/components/BlogTagsInput.tsx
@@ -1,11 +1,40 @@
-export default function BlogTagsInput({ value, onChange }: { value: string; onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+import { useState } from "react";
+import useEditorStore from "../store/editorStore";
+import { X } from "lucide-react";
+
+
+
+export default function BlogTagsInput() {
+  const [tagInput, setTagInput] = useState<string>()
+  const { tags, setStateValue } = useEditorStore();
+
+  const onKeyDownTagInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "," || e.key === "Enter") {
+      e.preventDefault();
+      const newTag = tagInput?.trim();
+      if (newTag && !tags.includes(newTag) && tags.length < 4) {
+        setStateValue({ name: "tags", value: [...tags, newTag] })
+      }
+      setTagInput("");
+    }
+  }
   return (
-    <input
-      className="bg-accent/10 px-3 py-5 outline-0 focus:outline-1 focus:outline-accent rounded-md"
-      value={value}
-      onChange={onChange}
-      name="tags"
-      placeholder="Tags (comma-separated)..."
-    />
+    <div className="w-full grid md:flex gap-2 bg-accent/10 px-3 py-5 ">
+      <div className="flex flex-none gap-1">
+        {tags && tags.map(tag => (
+          <div key={tag} className="bg-accent px-1 py-0.5 rounded-md flex items-center gap-1">{tag} <X strokeWidth={5} className="h-3 w-3 cursor-pointer hover:text-red-500" /></div>
+        ))}
+      </div>
+      {tags.length < 4 && (
+        <input
+          className="outline-0 rounded-md w-full"
+          value={tagInput}
+          onChange={(e) => setTagInput(e.target.value)}
+          onKeyDown={onKeyDownTagInput}
+          name="tags"
+          placeholder="Tags (comma-separated)..."
+        />
+      )}
+    </div>
   );
 }
