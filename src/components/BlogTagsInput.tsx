363687b55ccd36d19198da3c26189cb88eea24d1
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-30 11:49:05 +0530

    remove tags option added

--- src/components/BlogTagsInput.tsx
+++ src/components/BlogTagsInput.tsx
@@ -5,7 +5,7 @@ import { X } from "lucide-react";
 
 
 export default function BlogTagsInput() {
-  const [tagInput, setTagInput] = useState<string>()
+  const [tagInput, setTagInput] = useState<string>("")
   const { tags, setStateValue } = useEditorStore();
 
   const onKeyDownTagInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -18,11 +18,16 @@ export default function BlogTagsInput() {
       setTagInput("");
     }
   }
+  const removeTag = (tagToRemove: string) => {
+    setStateValue({ name: "tags", value: tags.filter(tag => tag !== tagToRemove) })
+  }
   return (
     <div className="w-full grid md:flex gap-2 bg-accent/10 px-3 py-5 ">
       <div className="flex flex-none gap-1">
         {tags && tags.map(tag => (
-          <div key={tag} className="bg-accent px-1 py-0.5 rounded-md flex items-center gap-1">{tag} <X strokeWidth={5} className="h-3 w-3 cursor-pointer hover:text-red-500" /></div>
+          <div key={tag} className="bg-accent px-1 py-0.5 rounded-md flex items-center gap-1">
+            {tag} <X onClick={() => removeTag(tag)} strokeWidth={5} className="h-3 w-3 cursor-pointer hover:text-red-500" />
+          </div>
         ))}
       </div>
       {tags.length < 4 && (
@@ -33,6 +38,7 @@ export default function BlogTagsInput() {
           onKeyDown={onKeyDownTagInput}
           name="tags"
           placeholder="Tags (comma-separated)..."
+          autoComplete="off"
         />
       )}
     </div>
